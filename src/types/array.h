31839acb570a47cf90953e2b185671508dfa4775
Luis Rodrigues da Silva <luisr.silva@outlook.com>
2025-11-17 00:29:12 -0300

    atualizando array.h para construtor ficar fora da struct

--- src/types/array.h
+++ src/types/array.h
@@ -1,37 +1,37 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-#include "stdbool.h"
+#include <stdbool.h>
 #include <stdio.h>
 
 // array is a pointer to Array struct
 typedef struct Array *array;
 
 // Functions pointers to
-typedef array (*Init)(unsigned int size);
 typedef bool (*Empty)(array self);
 typedef void (*Add)(array self, unsigned int element);
 typedef void (*Remove)(array self, unsigned int index);
+typedef void (*Destroy)(array self);
 
 /**
  * @brief: The basic structure of a dynamic array of unsigned int
  * @param vector -> Pointer to hold the elements of array
  * @param capacity -> Number of elements the array can hold in the current time
  * @param size -> Current total of elements hold by array
- * @param init -> pointer to function initArray;
  * @param is_empty -> pointer to function isEmpty;
  * @param remove -> pointer to function removeElement;
  * @param add -> pointer to function addElement;
+ * @param destroy -> pointer to function destroyArray
  **/
 struct Array {
   unsigned int *vector;
   size_t capacity;
   size_t size;
 
-  Init init;
   Empty is_empty;
   Remove remove;
   Add add;
+  Destroy destroy; 
 };
 
 /**
@@ -59,5 +59,11 @@ void removeElement(array self, unsigned int index);
  * @return false -> size > 0
  */
 bool isEmpty(array self);
+
+/**
+ * @brief: Deletes all content from array
+ * @param self -> Pointer to array itself
+*/
+void destroyArray(array self);
 
 #endif
