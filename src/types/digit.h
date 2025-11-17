e42e5387b5cff30f17e64aa9ce21d10e7ec18903
Luis Rodrigues da Silva <luisr.silva@outlook.com>
2025-11-17 00:30:25 -0300

    refactor: digit.h agora como biblioteca auxiliar

--- src/types/digit.h
+++ src/types/digit.h
@@ -7,14 +7,4 @@
 static inline int charToInt(char c) { return isdigit(c) ? c - '0' : -1; }
 static inline char intToChar(int i) { return (char)i + '0'; }
 
-typedef struct Digit *digit;
-struct Digit {
-  digit nxt;
-  digit prv;
-  char digit_value;
-  char carry;
-};
-
-digit initDigit(char d);
-
 #endif
