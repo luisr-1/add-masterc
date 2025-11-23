8ce40ea3bf9ae4080cd9918f2441a1323e03adad
arthurht122 <el_brabo@gmail.com>
2025-11-22 21:43:12 -0300

    Adicionando funcao de multiplicacao

--- src/main.c
+++ src/main.c
@@ -1,43 +1,25 @@
-#include "calc/core/operation.h"
-#include "calc/handlers/handle_operation.h"
-#include "view/menu.h"
-
+#include "calc/core/mult.h"
+#include <stdlib.h>
 #include <stdio.h>
-
-void clear_input_buffer() {
-  int c;
-  while ((c = getchar()) != '\n' && c != EOF)
-    ;
-}
-
-int main(void /*int argc, char **argv*/) {
-  show_menu();
-  char operator;
-  int a, b;
-
-  do {
-    printf("Insira um operador: \n");
-    scanf("%c", &operator);
-    clear_input_buffer();    
-    
-    if(operator != '!'){
-        printf("Insira dois números para realizar a operação: \n");
-        scanf("%d%d", &a, &b);
-        clear_input_buffer();
-
-        printf("O resultado de %d %c %d = %d\n", a, operator, b,
-           get_result(operator, a, b));
+ 
+int main() {
+    // Teste com números grandes
+    bigInt num1 = initNumber("89999999999");
+    bigInt num2 = initNumber("98888888888");
+ 
+    if (num1 && num2) {
+        bigInt result = multBigInt(num1, num2);
+ 
+        if (result) {
+            char* result_str = result->to_decimal_representation(result);
+            printf("Resultado: %s\n", result_str);
+            free(result_str);
+            result->destroy(result);
+        }
+ 
+        num1->destroy(num1);
+        num2->destroy(num2);
     }
-    else{
-
-         printf("Insira um numero para realizar a operacao: \n");
-         scanf("%d", &a);
-         clear_input_buffer();
-
-         printf("O resultado de %d%c = %d\n", a, operator, 
-                               get_result(operator, a, 1));
-    }
-  } while (is_valid_operation(operator));
-
-  return 0;
+ 
+    return 0;
 }
